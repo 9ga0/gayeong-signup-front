d061ab6617b5c9b4ea5e2bb0ccf421f9dbd8aa09
gaga00543 <koo050803+1@gmail.com>
2026-05-01 01:01:14 +0900

    로그인 화면 구조 설계

--- client/signup-project/src/App.jsx
+++ client/signup-project/src/App.jsx
@@ -5,42 +5,26 @@ function App() {
   return (
     <>
       <div id="background_gradient">  
-        <main id="signup_box"> {/* 회원가입 박스 */}
-          <h1>회원가입</h1> 
-          <form id="input_box">
-            <div class="input_box">
-              <p>이메일<br/> </p>
-              <input id="input" type="email" placeholder="이메일"/><br/>
-              <button
-                type="button"
-                className="email_button"
-              >
-                 전송
-              </button>
-              <input id="input" type="text" placeholder="인증번호" />
+        <main id="signup_box"> 
+          <h1>En# SignUp!!</h1> 
+
+          <form class="input_box">
+            <input id="input" type="email" placeholder="E-mail"/>
+            <input id="input" type="password" placeholder="Password"/>
+            <div id="line_box">
+              <input id="checkbox" type="checkbox"/>아이디 저장
+              <p>비밀번호 찾기</p>
             </div>
-            <div class="input_box">
-              <p>비밀번호<br/> </p>
-              <input id="input" type="password" placeholder="8~16자의 영문 대/소문자, 숫자, 특수문자"/><br/>
-              <input id="input" type="password" placeholder="비밀번호 확인"/>
-            </div>
-            <div class="input_box">
-              <p>이름<br/> </p>
-              <input id="input" type="text"/>
-            </div>
-            <div class="input_box">
-              <p>주소<br/> </p>
-              <input id="input" type="address" placeholder="클릭하여 주소 검색"/><br/>
-              <input id="input" type="text" placeholder="상세주소"/>
-            </div>
+
           </form>
-        
-          <button
-            type="submit"
+
+            
+          <button 
+            type="button"
             className="submit_button"
-          >
-            제출하기
-          </button>
+            >Login</button>
+
+          <p>계정이 없으신가요?</p> 회원가입
         </main>
       </div>
 
---
+++ client/signup-project/src/SignUpScene.jsx
@@ -0,0 +1,50 @@
+//import reactLogo from './assets/react.svg' //이미지 불러오기
+import './styles/SignUpScene.css' //css 불러오기
+
+function App() {
+  return (
+    <>
+      <div id="background_gradient">  
+        <main id="signup_box"> {/* 회원가입 박스 */}
+          <h1>회원가입</h1> 
+          <form class="input_box">
+              <p>이메일<br/> </p>
+                <input id="input" type="email" placeholder="이메일"/>
+                <button
+                  type="button"
+                  className="email_button"
+                >
+                 전송
+                </button>
+
+              <input id="input" type="text" placeholder="인증번호" />
+            <div class="input_box">
+              <p>비밀번호<br/> </p>
+              <input id="input" type="password" placeholder="8~16자의 영문 대/소문자, 숫자, 특수문자"/><br/>
+              <input id="input" type="password" placeholder="비밀번호 확인"/>
+            </div>
+            <div class="input_box">
+              <p>이름<br/> </p>
+              <input id="input" type="text"/>
+            </div>
+            <div class="input_box">
+              <p>주소<br/> </p>
+              <input id="input" type="address" placeholder="클릭하여 주소 검색"/><br/>
+              <input id="input" type="text" placeholder="상세주소"/>
+            </div>
+          </form>
+        
+          <button
+            type="submit"
+            className="submit_button"
+          >
+            제출하기
+          </button>
+        </main>
+      </div>
+
+    </>
+  )
+}
+
+export default App
