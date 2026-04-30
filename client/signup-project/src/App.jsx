ce57f0f6ee77d716fc301c465c614dc9529003f6
gaga00543 <koo050803+1@gmail.com>
2026-04-30 23:13:34 +0900

    회원가입 페이지 구조만 설계

--- client/signup-project/src/App.jsx
+++ client/signup-project/src/App.jsx
@@ -1,22 +1,47 @@
 //import reactLogo from './assets/react.svg' //이미지 불러오기
-import './App.css' //css 불러오기
+import './styles/App.css' //css 불러오기
 
 function App() {
   return (
     <>
-      <div class="background_gradient">  
-      <main id="signup_box"> {/* 회원가입 박스 */}
+      <div id="background_gradient">  
+        <main id="signup_box"> {/* 회원가입 박스 */}
           <h1>회원가입</h1> 
-
-
+          <form id="input_box">
+            <div class="input_box">
+              <p>이메일<br/> </p>
+              <input id="input" type="email" placeholder="이메일"/><br/>
+              <button
+                type="button"
+                className="email_button"
+              >
+                 전송
+              </button>
+              <input id="input" type="text" placeholder="인증번호" />
+            </div>
+            <div class="input_box">
+              <p>비밀번호<br/> </p>
+              <input id="input" type="password" placeholder="8~16자의 영문 대/소문자, 숫자, 특수문자"/><br/>
+              <input id="input" type="password" placeholder="비밀번호 확인"/>
+            </div>
+            <div class="input_box">
+              <p>이름<br/> </p>
+              <input id="input" type="text"/>
+            </div>
+            <div class="input_box">
+              <p>주소<br/> </p>
+              <input id="input" type="address" placeholder="클릭하여 주소 검색"/><br/>
+              <input id="input" type="text" placeholder="상세주소"/>
+            </div>
+          </form>
         
           <button
-            type="button"
-            className="submit"
+            type="submit"
+            className="submit_button"
           >
             제출하기
           </button>
-      </main>
+        </main>
       </div>
 
     </>
