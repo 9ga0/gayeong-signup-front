f54d87c8c1aa3e8f7093683fe32dbbb4ed6c3347
gaga00543 <koo050803+1@gmail.com>
2026-05-04 04:58:56 +0900

    fix/refactor: 회원가입 페이지에서 안 뜨는 컴포넌트 해결

--- client/signup-project/src/App.jsx
+++ client/signup-project/src/App.jsx
@@ -3,7 +3,7 @@ import './styles/App.css'
 import { BrowserRouter, Routes , Route } from "react-router-dom";
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
-//import FindPassWord from './pages/FindPassWord';
+import FindPassWord from './pages/FindPassWord';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
             <Route path ="/" element = {< LogIn/>}/> 
             <Route path ="/sign-up" element = {<SignUp />}/>
-            {/* <Route path ="/find-password" element = {<FindPassWord />}/> */}
+            <Route path ="/find-password" element = {<FindPassWord />}/>
 
             {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
--- client/signup-project/src/pages/SignUp.jsx
+++ client/signup-project/src/pages/SignUp.jsx
@@ -14,39 +14,74 @@
 // [제출하기]
 import React from 'react';
 import CardTitle from "../components/common/Common"
-import InputBox from "../components/common/Common"
-import EmailBox from "../components/common/Common"
-import SubmitButton from "../components/common/Common"
+//import InputBox from "../components/common/Common"
+//import EmailBox from "../components/common/Common"
+//import SubmitButton from "../components/common/Common"
 import '../components/common/Common.css';
+function InputBox(children){
+    return(
+      <div id="input_box">
+        {children}
+      </div>
+    )
+}
+function EmailBox(){
+    return(
+        <>
+          <p>이메일<br/> </p>
+          <div>
+            <button
+                type="button"
+                className="email_button"
+                >
+                전송
+            </button>
+            <input id="input" placeholder="이메일"/>
+          </div>
+            <input id="input" placeholder="인증번호" />
+        </>
+    )
+};
+function SubmitButton(props){
+  return(
+    <button
+          type="submit"
+          className="submit_button"
+            >
+            <p>{props.title}</p>
+        
+    </button>
+  )
+} 
 
 function SignUp() {
   return (
     <div id="background_gradient">
       <main id="card_box">
-          <div>
-            <CardTitle title={props.title} />
-              <div id="inputBox">
+            <CardTitle title="회원가입" />
+            <div id="input_box">
+              <div id="input_wrap">
                 <EmailBox/>
               </div>
 
-              <div id="inputBox">
+              <div id="input_wrap">
                 <p>비밀번호<br/> </p>
                 <input id="input" type="password" placeholder="8~16자의 영문 대/소문자, 숫자, 특수문자"/><br/>
                 <input id="input" type="password" placeholder="비밀번호 확인"/>
               </div>
 
-              <div id="inputBox">
+              <div id="input_wrap">
                 <p>이름<br/> </p>
                 <input id="input" type="text"/>
               </div>
 
-              <div id="inputBox">
+              <div id="input_wrap">
                 <p>주소<br/> </p>
                 <input id="input" type="address" placeholder="클릭하여 주소 검색"/><br/>
                 <input id="input" type="text" placeholder="상세주소"/>
               </div>
-          </div>
-          <SubmitButton title="제출하기" />
+            </div>
+            <SubmitButton title="제출하기" />
       </main>
     </div>
   );
