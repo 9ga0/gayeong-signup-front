cd553bf47bb13451540ee36cfaaedfba5b41a73f
9ga0 <koo050803@gmail.com>
2026-05-14 02:11:44 +0900

    feat: 비밀번호 일치판단 버그 수정
    - 순차적으로 pw 유효하게 작성하고, confirmPw 동일하게 작성하면 버그 없지만, 반대로 하거나 값 변경시 버그존재한다.

--- client/signup-project/src/components/common/PasswordBox.jsx
+++ client/signup-project/src/components/common/PasswordBox.jsx
@@ -10,20 +10,30 @@ export default function PasswordBox() {
         pw: '',
         confirmPw: ''
     })
+    const initState = {
+        pw: '',
+        confirmPw: ''
+    }
+    const [registerParam, setRegisterParam] = useState({ ...initState })
+
 
     return (
         <>
             <div className="sub-title">비밀번호</div>
             <PasswordInput type="input" name='pw'
                 placeholder="8~16자의 영문 대/소문자, 숫자, 특수문자"
-                onSetErrors={setErrors} 
+                onSetErrors={setErrors}
                 errors={errors}
-                img={EyeLock}/>
+                registerParam={registerParam}
+                setRegisterParam={setRegisterParam}
+                img={EyeLock} />
             <PasswordInput type="input" name='confirmPw'
                 placeholder="비밀번호 확인"
-                onSetErrors={setErrors} 
-                errors={errors} 
-                img={EyeLock}/>
+                onSetErrors={setErrors}
+                errors={errors}
+                registerParam={registerParam}
+                setRegisterParam={setRegisterParam}
+                img={EyeLock} />
             {errors.pw && <div className='error'>{errors.pw}</div>}
             {errors.confirmPw && <div className='error'>{errors.confirmPw}</div>}
         </>
--- client/signup-project/src/components/common/PasswordInput.jsx
+++ client/signup-project/src/components/common/PasswordInput.jsx
@@ -4,13 +4,9 @@ import EyeLock from "../../assets/EyeLock.svg"
 import EyeUnLock from "../../assets/EyeUnLock.svg"
 
 export default function PasswordInput(props) {
-    const initState = {
-        pw: '',
-        confirmPw: ''
-    }
     const [visible, setVisible] = useState(false);
     const [imageSrc, setImageSrc] = useState(props.img);
-    const [registerParam, setRegisterParam] = useState({ ...initState })
+
     const handleClick = () => {
         if (visible) {
             setImageSrc(EyeLock);
@@ -26,7 +22,7 @@ export default function PasswordInput(props) {
         let error = ''
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
         // 하나 이상의 대문자, 하나 이상의 소문자, 하나이상의 숫자, 하나이상의 특수문자 가 8자~16자로 구성
-        switch (name) { 
+        switch (name) {
             case 'pw':
                 if (!value) {
                     error = '비밀번호를 입력해주세요.'
@@ -36,7 +32,7 @@ export default function PasswordInput(props) {
                     error = '8~16자의 영문 대/소문자, 숫자, 특수문자'
                     setBorderColor('#EE4346A6');
                 }
-                else if(passwordRegex.test(value)){//통과
+                else if (passwordRegex.test(value)) {//통과
                     setBorderColor('#435DEEA6');
                 }
                 else {
@@ -48,10 +44,10 @@ export default function PasswordInput(props) {
                     error = '비밀번호가 일치하지 않습니다.'
                     setBorderColor('#EE4346A6');
                 }
-                else if(value) { //비밀번호 일치
+                else if (value) { //비밀번호 일치
                     setBorderColor('#435DEEA6');
                 }
-                else{
+                else {
                     setBorderColor('#89848466');
                 }
                 break
@@ -64,14 +60,14 @@ export default function PasswordInput(props) {
     const handleChange = (e) => {
         const { name, value } = e.target;
 
-        setRegisterParam({
-            ...registerParam,
+        props.setRegisterParam({
+            ...props.registerParam,
             [name]: value
         });
 
         let error
         if (e.target.name === 'confirmPw') { // confirmPw 필드에 입력할 때마다 현재 pw 필드의 값과 비교하여 에러 체크
-            error = validateField(name, value, registerParam.pw)
+            error = validateField(name, value, props.registerParam.pw)
         } else { //confirm 필드가 비어있을 때는 에러 메시지를 표시하지 않음
             error = validateField(name, value)
         }
@@ -80,23 +76,24 @@ export default function PasswordInput(props) {
             [name]: error
         })
 
+        console.log('pw:', props.registerParam.pw, 'confirmpw:', props.registerParam.confirmPw)
         // pw가 변경되었을 때도 confirmPw 필드의 값을 재검증하여 에러 상태 업데이트
-        if (e.target.name === 'pw' && registerParam.confirmPw) {
-            const confirmPwError = validateField('confirmPw', registerParam.confirmPw, value)
+        if (e.target.name === 'pw' && props.registerParam.confirmPw) {
+            const confirmPwError = validateField('confirmPw', props.registerParam.confirmPw, value)
             props.onSetErrors(prev => ({
                 ...prev,
                 confirmPw: confirmPwError
             }))
         }
 
-        if(imageSrc!==EyeUnLock) setImageSrc(EyeLock);
-        props.isLogin? setBorderColor('#89848466'): null;
+        if (imageSrc !== EyeUnLock) setImageSrc(EyeLock);
+        props.isLogin ? setBorderColor('#89848466') : null;
     }
 
 
     return (
         <div className="input-container"
-             style={{ border: `2px solid ${borderColor}` }}>
+            style={{ border: `2px solid ${borderColor}` }}>
             <input className="input2"
                 name={props.name}
                 type={visible ? "text" : "password"}
