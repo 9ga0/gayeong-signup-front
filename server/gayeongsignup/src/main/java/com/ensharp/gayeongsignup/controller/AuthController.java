f645b1664c59781aa052481731fccd6a752083d1
9ga0 <koo050803@gmail.com>
2026-05-28 12:34:43 +0900

    fix: 서버 연결을 위해 yaml 파일 수정 & 회원가입 dto 반환안 되는 버그 수정

--- server/gayeongsignup/src/main/java/com/ensharp/gayeongsignup/controller/AuthController.java
+++ server/gayeongsignup/src/main/java/com/ensharp/gayeongsignup/controller/AuthController.java
@@ -54,9 +54,9 @@ public class AuthController {
                     ))
     })
     @PostMapping("/signup")
-    public ResponseEntity<String> join(@RequestBody @Valid SignupRequestDto signupRequestDto) throws Exception {
+    public ResponseEntity<SignupRequestDto> join(@RequestBody @Valid SignupRequestDto signupRequestDto) throws Exception {
         System.out.println("회원가입 요청이 들어옴 : " + signupRequestDto.email());
-        String result = memberServiceImpl.join(signupRequestDto); //리턴값은 바디로 출력됨
+        SignupRequestDto result = memberServiceImpl.join(signupRequestDto); //리턴값은 바디로 출력됨
 
         //if("success".equals(result)){
         //회원가입 성공시에만 동작. 그 전에 service에서 예외 던져짐
